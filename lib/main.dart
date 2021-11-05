95b5adff2b181a237436d653ac743ad69179e34d
t-nakata <toshihiro.nakata@axia-works.com>
2021-11-06 00:26:21 +0900

    全画面、縦固定を導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flame/puzzle_game.dart';
 
-void main() {
+void main() async {
   runApp(GameWidget(game: PuzzleGame()));
+  await Flame.device.fullScreen();
+  await Flame.device.setPortrait();
 }
